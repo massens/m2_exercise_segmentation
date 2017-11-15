8085156c9b1caf4a671a02426f5ad625f5edb4af
Marc Assens <marc.a.r95@gmail.com>
2017-11-15 17:33:29 +0100

    Initial commit

---
+++ CreateGridUGMModel.m
@@ -0,0 +1,42 @@
+function [edgePot,edgeStruct]=CreateGridUGMModel(NumFils, NumCols, K, lambda)
+%
+%
+% NumFils, NumCols: image dimension
+% K: number of states
+% lambda: smoothing factor
+
+
+
+
+%% Represent denoising as UGM
+
+nNodes = NumFils*NumCols;
+adj = sparse(nNodes,nNodes);
+
+% Add Down Edges
+ind = 1:nNodes;
+exclude = sub2ind([NumFils NumCols],repmat(NumFils,[1 NumCols]),1:NumCols); % No Down edge for last row
+ind = setdiff(ind,exclude);
+adj(sub2ind([nNodes nNodes],ind,ind+1)) = 1;
+
+% Add Right Edges
+ind = 1:nNodes;
+exclude = sub2ind([NumFils NumCols],1:NumFils,repmat(NumCols,[1 NumFils])); % No right edge for last column
+ind = setdiff(ind,exclude);
+adj(sub2ind([nNodes nNodes],ind,ind+NumFils)) = 1;
+
+% Add Up/Left Edges
+adj = adj+adj';
+edgeStruct = UGM_makeEdgeStruct(adj,K);
+
+
+% Define edgePot
+
+
+
+tic
+
+edgePot=[];
+edgeStruct=[];
+
+toc;
